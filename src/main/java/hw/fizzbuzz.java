77c34fa4d25a110438c88bb6cfd8afd6ec4ddcc1
Eththenet <ethannmohan@gmail.com>
2017-09-08 09:40:20 -0500

    Create fizzbuzz.java

---
+++ src/main/java/hw/fizzbuzz.java
@@ -0,0 +1,21 @@
+public class fizzbuzz {
+    public static void main(String[] args) {
+        int n = 18, i;
+
+
+        for(i=0; i < n; i++) {
+            if(i % 15 == 0){
+                System.out.println( "fizzbuzz");
+            }
+            else if((i % 5) == 0){
+                System.out.println("buzz");
+            }
+            else if(( i % 3) == 0){
+                System.out.println("fizz");
+            }
+            else{
+                System.out.println(i);
+            }
+        }
+    }
+}
