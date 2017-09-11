b95a5e71af022a5e6692f189f03cc1c8412487a0
ncarsello <31541956+ncarsello@users.noreply.github.com>
2017-09-11 12:07:10 -0500

    Create lab1-part2.java
    With user input

---
+++ src/main/java/hw/lab1-part2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class lab1-part2 {
+    public static void main(String[] args) {
+       Scanner keyboard = new Scanner (System.in);
+	   System.out.println("Please enter a number and I will make a Fizz buzz sequence for you");
+       int n = keyboard.nextInt();
+       int i;
+       for(i=0; i < n; i++) {
+            if(i % 15 == 0){
+                System.out.println( "fizzbuzz");
+           }
+            else if((i % 5) == 0){
+                System.out.println("buzz");
+            }
+            else if(( i % 3) == 0){
+                System.out.println("fizz");
+            }
+            else{
+                System.out.println(i);
+            }
+        }
+    }
+}
